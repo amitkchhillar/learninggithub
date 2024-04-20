41947184b6616a1a76b3a452aa39dc3cd2bf8b91
amitkchhillar <amit.chhillar@rocketmail.com>
2024-04-20 13:35:32 +0530

    adding two numbers

--- basic.java
+++ basic.java
@@ -1,10 +1,19 @@
+
 /**
  * basic
  */
 public class basic {
-    public static void main (String a[])
+    /**
+     * @param args
+     */
+    public static void main (String args[])
     {
-        System.out.println("Hello World");
+       double x =6;
+       double y =6;
+       boolean result = x == y;
+       System.out.println(result);
+
+        
     }
 }
 
